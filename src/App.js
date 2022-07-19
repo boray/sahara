839fa2b71f1e70abced1486bef60573ad6202ff6
“boray” <bsaygilier33@gmail.com>
2022-07-19 11:35:57 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(3),
+  textAlign: 'center',
+  color: theme.palette.text.primary,
+}));
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Type Class or Library Address" variant="outlined" />
+      <Button className="queryButton" variant="contained">Query</Button>
+       </Box>
+       <Box sx={{ width: '70%', paddingTop:'5ch' }}>
+      <Stack spacing={2}>
+        <Item>Contructor(parameterOne: felt, parameterTwo: felt, parameterThree: felt, parameterFour: felt)</Item>
+        <Item>@external MultiplyTwoNums(firstNumber: felt, secondNumber: felt) -> (result: felt)</Item>
+        <Item>@external SubtractTwoNums(firstNumber: felt, secondNumber) -> (result: felt)</Item>
+        <Item>@external AddTwoNums(parameterOne: felt, parameterTwo: felt) -> (result: felt)</Item>
+        <Item>@view SupportsInterface(interfaceId: felt) -> (success: felt)</Item>
+      </Stack>
+    </Box>
       </header>
+      
+  
+    
     </div>
   );
 }
