ded25eb5ab8086822296a035601bc288d7789e19
“boray” <bsaygilier33@gmail.com>
2022-09-18 17:13:50 +0300

    improved listing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
-import React, { useEffect, Paper, FlatList,Text, Divider } from 'react';
+import React from 'react';
 import axios from 'axios';
-import { getAllByTestId } from '@testing-library/react';
-import { ListItem, ListItemText } from '@mui/material';
+
 
 
 
@@ -10,7 +9,6 @@ function App() {
 
   const [classHash, setClassHash] = React.useState();
   const [Response, setResponse] = React.useState([]);
-  const [errorMessage, setErrorMessage] = React.useState();
   const [functionsArray, setFunctionsArray] = React.useState([]);
   const [structsArray, setStructsArray] = React.useState([]);
   const [eventsArray, setEventsArray] = React.useState([]);
@@ -35,13 +33,13 @@ function App() {
     let structs = [];
     let events = [];
     for(let i = 0; i< array.length; i++){
-      if(array[i].type == "function"){
+      if(array[i].type === "function"){
         functions.push(array[i]);
       }
-      else if(array[i].type == "event"){
+      else if(array[i].type === "event"){
         events.push(array[i]);
       }
-      else if(array[i].type == "struct"){
+      else if(array[i].type === "struct"){
         structs.push(array[i]);
       }
     }
@@ -51,15 +49,20 @@ function App() {
 
   }
 
+// WANTED BEHAVOUR
+// struct structName(x: felt, y:felt, z: AnotherStruct)
+// event eventName(x: felt, y: felt)
+// @view function functionName(x : felt, y :felt) -> (a: felt, b:felt)
 
 
   return (
     <div className="App">
       <header className="App-header">
-      <h1> Starknet Class Library Query</h1>
+      <h1> Sahara: Starknet Contract Class Visualizer</h1>
       <form onSubmit={handleSubmit}>
-      <p>What is contract classes https://docs.starknet.io/docs/Contracts/contract-classes/</p>
+      <p>Cairo 0.9 splitted the concepts of contract class and contract instance. By this, anyone can declare a contract class and anyone can deploy an instance of that contract class.</p>
       <input
+        className="queryField"
         type="text"
         name="classhash"
         placeholder="Enter a class hash"
@@ -71,34 +74,97 @@ function App() {
       </button>
     </form>
 
+    </header>
+
+    <div className="result">
+      
+
+
    {functionsArray.length > 0 &&(
-    <ul>
+    <ol>
    {functionsArray.map(item =>(
-    <li key={item.id}>{item.name} -- {item.type}
-    {
-    item.outputs.map(output =>(
-     <p> <li key={output.id}>{output.name} - {output.type}
-     {
-    item.inputs.map(input =>(
-     <p> <li key={input.id}>{input.name} - {input.type}</li></p>
-    ))
-    }
+    <li>
     {
       item.hasOwnProperty("stateMutability") && (
-        <p>
-          {item.stateMutability}
-        </p>
+        
+          <>@ {item.stateMutability} </>
+       
       )
-    }</li></p>
+    }
+      
+      function {item.name}(
+    
+    
+    {
+    item.inputs.map((input,i) =>(
+      <>{i > 0 && <>,</>} {input.name}: {input.type}</> 
     ))
     }
-    
+      )
+
+-> (
+    {
+    item.outputs.map((output,i) =>(
+      <>{i > 0 && <>,</>} {output.name}: {output.type}</>
+    ))
+    }
+
+)
+
     </li>
    ))}
-   </ul>
+   </ol>
     )}
 
-    </header>
+
+{eventsArray.length > 0 &&(
+    <ol>
+   {eventsArray.map(item =>(
+    <li>
+  
+      
+      event {item.name}(
+    
+    
+    {
+    item.data.map((data,i) =>(
+      <>{i > 0 && <>,</>} {data.name}: {data.type}</> 
+    ))
+    }
+    )
+
+
+    </li>
+   ))}
+   </ol>
+    )}
+
+{structsArray.length > 0 &&(
+    <ol>
+   {structsArray.map(item =>(
+    <li>
+  
+      
+      struct {item.name}(
+    
+    
+    {
+    item.members.map((data,i) =>(
+      <>{i > 0 && <>,</>} {data.name}: {data.type}</> 
+    ))
+    }
+    )
+
+
+    </li>
+   ))}
+   </ol>
+    )}
+
+
+
+
+</div>
     </div>
   );
 }
